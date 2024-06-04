8043efa752b825aa75390bcf807707171911a052
Saneep dalal <171ankit.2020@gmail.com>
2024-06-04 06:27:55 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css'; // Import the CSS file
+import './App.css'; 
 import WeatherCard from './WeatherCard';
 
 function App() {
